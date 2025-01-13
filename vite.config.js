4367131af5b9584b6596ab681acb53468481fee9
Amen Kaabachi <amenallahkaabachi@gmail.com>
2025-01-13 17:21:32 +0100

    fix: update base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/analytics/', // Ensure this matches the repository name if deploying to GitHub Pages
+  base: '/user-analytics/', // Ensure this matches the repository name if deploying to GitHub Pages
   build: {
     outDir: 'dist',
     emptyOutDir: true
