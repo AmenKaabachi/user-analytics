f0b08b20c7a9f90939945ad940870502a8e90f04
Amen Kaabachi <amenallahkaabachi@gmail.com>
2025-01-13 17:17:16 +0100

    Update Vite configuration to set output directory and ensure base path matches repository name

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/analytics/",
+  base: '/analytics/', // Ensure this matches the repository name if deploying to GitHub Pages
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  },
 })
